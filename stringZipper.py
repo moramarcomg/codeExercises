b96bc13a0b6fec33c3d5f25ddebdaf8b06425657
moramarcomg <mora.marcomg@gmail.com>
2026-05-27 00:35:02 -0600

    feat: add coding exercises to src-exercises folder

--- stringZipper.py
+++
@@ -1,9 +0,0 @@
-# String Zipper
-# Given two strings, return a new string that interleaves their characters one at a time. If one string is longer, append the remaining characters at the end.
-
-# Begin with the first character of the first string.
-
-from itertools import zip_longest
-
-def zip_strings(a, b):
-   return "".join(c1 + c2 for c1, c2 in zip_longest(a, b, fillvalue=""))
